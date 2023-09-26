5ce86c11bb9f8791c17d06e4505761afe7c8a887
Rasalahmad <rasalahmadsit@gmail.com>
2023-09-26 13:43:40 +0600

    wip

--- src/pages/userList/serviceApi.js
+++ src/pages/userList/serviceApi.js
@@ -5,7 +5,7 @@ import { publicRequest } from "../../requestMethods";
 export const login = async (dispatch, user) => {
   dispatch(loginStart());
   try {
-    const res = await publicRequest.post("/auth/login", user);
+    const res = await publicRequest.post("/auth/login/dashboard", user);
     dispatch(loginSuccess(res.data.data));
     Swal.fire({
       icon: "success",
